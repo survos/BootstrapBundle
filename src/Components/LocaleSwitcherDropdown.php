8cfe838feabcc223bb8902472fd003d7d12219c3
tacman <info@survos-ci.com>
2025-01-29 11:54:33 +0000

    cleanup messages

--- src/Components/LocaleSwitcherDropdown.php
+++ src/Components/LocaleSwitcherDropdown.php
@@ -10,8 +10,8 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 final class LocaleSwitcherDropdown
 {
 //    public bool $putLocaleInSubdomain = false;
-    public string $localeInRequest; // the local that's in the current request
-    public array $localLinks=[];
+    public ?string $localeInRequest=null; // the local that's in the current request
+    public array $localeLinks=[];
 
     public function __construct(
         private RequestStack $requestStack,
@@ -31,18 +31,18 @@ final class LocaleSwitcherDropdown
         $request = $this->getRequest();
         $host = $this->getRequest()->getHttpHost();
         $hostParts = explode(".", $host);
-        $alt = [];
-        if (count($hostParts) === 3) {
-            $uri = $request->getUri();
+        if (count($hostParts) === 3)
+        {
+            $uri = $request->getUri(); // the full url
             $this->localeInRequest = array_shift($hostParts);
             foreach ($this->enabledLocales as $subdomain) {
                 $search = "https://{$this->localeInRequest}.";
                 $replace = "https://$subdomain.";
 //                dump($uri, $search, $subdomain);
 //                dump($this->localeInRequest, $subdomain);
-                $this->localeLinks[$subdomain] = str_replace($search, $replace, $uri);
+                $this->localeLinks[$subdomain] = str_replace($search, $replace,
+                    $uri);
             }
-//            dd($this->localeLinks);
         }
     }
 
